7eb63979aaaf5d8100c3523034d9905ef5f0ccb8
Ningareddy Bheemannagari <ningareddy987@gmail.com>
2025-02-22 20:21:04 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const element = (
-  <div>
-    <h1 className='greeting'>Hello World!!</h1>
-    <p>good to see you</p>
-    <button>click here</button>
-  </div>
-)
-ReactDOM.render(element, document.getElementById('root'))
