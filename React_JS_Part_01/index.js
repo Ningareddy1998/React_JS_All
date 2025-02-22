3582ceb48be288dbf70b0b852c41c311ecab46c6
Ningareddy <ningareddy987@gmail.com>
2025-02-22 03:31:52 +0000

    This is Part 01

---
+++ React_JS_Part_01/index.js
@@ -0,0 +1,8 @@
+const element = (
+  <div>
+    <h1 className='greeting'>Hello World!!</h1>
+    <p>good to see you</p>
+    <button>click here</button>
+  </div>
+)
+ReactDOM.render(element, document.getElementById('root'))
