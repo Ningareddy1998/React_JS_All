023d28cbf51efc5111410fc48dcbf263a9089151
Ningareddy <ningareddy@gmail.com>
2025-02-22 14:50:03 +0000

    commit message

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const element = (
   <div>
     <h1 className='greeting'>Hello World!!</h1>
     <p>good to see you</p>
+    <button>click here</button>
   </div>
 )
 ReactDOM.render(element, document.getElementById('root'))
