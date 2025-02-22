fa13965d1b88018149a39d7c84823041e4acc0ad
Ningareddy Bheemannagari <ningareddy987@gmail.com>
2025-02-22 20:33:42 +0530

    CP-01

---
+++ CP_01_PART_01/index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className='bg-container'>
+    <h1 className='heading'>Super Over Leage</h1>
+    <div>
+      <img
+        src='https://assets.ccbp.in/frontend/react-js/rcb-img.png'
+        className='image-1'
+      />
+      <img
+        src='https://assets.ccbp.in/frontend/react-js/csk-img.png'
+        className='image-2'
+      />
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
