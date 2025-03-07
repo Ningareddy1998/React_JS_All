e1fa3cbcc9028047d8e46855f859f95d5b33b500
Ningareddy Bheemannagari <ningareddy987@gmail.com>
2025-03-07 17:13:33 +0530

    CP-23

---
+++ CP_23_PART/src/components/BlogList/index.js
@@ -0,0 +1,12 @@
+import BlogItem from '../BlogItem'
+import './index.css'
+
+const BlogList = ({blogsList}) => (
+  <ul className="blog-list">
+    {blogsList.map(blog => (
+      <BlogItem key={blog.id} blog={blog} />
+    ))}
+  </ul>
+)
+
+export default BlogList
