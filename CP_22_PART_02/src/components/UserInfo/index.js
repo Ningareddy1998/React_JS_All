b175adf5a2281855829e91a78f39796ab0372840
Ningareddy Bheemannagari <ningareddy987@gmail.com>
2025-03-07 11:15:53 +0530

    CP_22_02

---
+++ CP_22_PART_02/src/components/UserInfo/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const UserInfo = () => (
+  <div className="user-info">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      alt="profile"
+      className="user-image"
+    />
+    <h1 className="user-name">Wade Warren</h1>
+    <p className="user-role">Software Developer at UK</p>
+  </div>
+)
+export default UserInfo
