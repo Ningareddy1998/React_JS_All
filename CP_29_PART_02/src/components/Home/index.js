45059128f274be2162019d639f9cc0d0f0605bec
Ningareddy Bheemannagari <ningareddy987@gmail.com>
2025-03-12 11:56:44 +0530

    CP-29

---
+++ CP_29_PART_02/src/components/Home/index.js
@@ -0,0 +1,13 @@
+// src/components/Home/index.js
+import Header from '../Header'
+import LogoutButton from '../LogoutButton'
+
+const Home = () => (
+  <div>
+    <Header />
+    <h1>Home Route</h1>
+    <LogoutButton />
+  </div>
+)
+
+export default Home
