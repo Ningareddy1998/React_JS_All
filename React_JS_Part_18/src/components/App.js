435e8803d2f7b6e3cfc293879d4f827adcd63db4
Ningareddy Bheemannagari <ningareddy987@gmail.com>
2025-03-12 18:53:46 +0530

    Part 18

--- React_JS_Part_18/src/components/App.js
+++ React_JS_Part_18/src/components/App.js
@@ -1,12 +1,25 @@
-* {
-  box-sizing: border-box;
-}
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
 
-body {
-  margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
+import LoginForm from './components/LoginForm'
+import Home from './components/Home'
+import Products from './components/Products'
+import Cart from './components/Cart'
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+
+import './App.css'
+
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/login" component={LoginForm} />
+      <ProtectedRoute exact path="/" component={Home} />
+      <ProtectedRoute exact path="/products" component={Products} />
+      <ProtectedRoute exact path="/cart" component={Cart} />
+      <Route path="/not-found" component={NotFound} />
+      <Redirect to="/not-found" />
+    </Switch>
+  </BrowserRouter>
+)
+
+export default App
