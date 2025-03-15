1b5b64229294d516fae59888de4ecfd98f431af5
Ningareddy Bheemannagari <ningareddy987@gmail.com>
2025-03-15 11:18:46 +0530

    CP_37

---
+++ CP_37_PART/src/components/VaccinationCoverage/index.js
@@ -0,0 +1,51 @@
+import {BarChart, Bar, XAxis, YAxis, Legend} from 'recharts'
+import './index.css'
+
+const VaccinationCoverage = props => {
+  const {vaccinationCoverageDetails} = props
+
+  const DataFormatter = number => {
+    if (number > 1000) {
+      return `${(number / 1000).toString()}k`
+    }
+    return number.toString()
+  }
+
+  return (
+    <div className="vaccination-coverage-container">
+      <h1 className="vaccination-coverage-heading">Vaccination Coverage</h1>
+      <BarChart
+        width={1000}
+        height={300}
+        data={vaccinationCoverageDetails}
+        margin={{
+          top: 5,
+        }}
+      >
+        <XAxis
+          dataKey="vaccineDate"
+          tick={{
+            stroke: 'gray',
+            strokeWidth: 1,
+          }}
+        />
+        <YAxis
+          tickFormatter={DataFormatter}
+          tick={{
+            stroke: 'gray',
+            strokeWidth: 0,
+          }}
+        />
+        <Legend
+          wrapperStyle={{
+            padding: 30,
+          }}
+        />
+        <Bar dataKey="dose1" name="Dose 1" fill="#5a8dee" barSize="20%" />
+        <Bar dataKey="dose2" name="Dose 2" fill="#f54394" barSize="20%" />
+      </BarChart>
+    </div>
+  )
+}
+
+export default VaccinationCoverage
