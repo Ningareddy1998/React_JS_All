d88dcb1787433227e43420a0756bcefa49c98b36
Ningareddy Bheemannagari <ningareddy987@gmail.com>
2025-03-25 22:24:06 +0530

    Part 26

---
+++ React_JS_Part_26/src/App.js
@@ -0,0 +1,13 @@
+import {Heading, CustomButton} from './styledComponents'
+
+const App = () => (
+  <>
+    <Heading>Hello</Heading>
+    <CustomButton type="button">Add</CustomButton>
+    <CustomButton type="button" outline>
+      Add
+    </CustomButton>
+  </>
+)
+
+export default App
